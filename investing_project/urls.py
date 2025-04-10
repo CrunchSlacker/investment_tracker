2681787dd0bc13869291ea12c86f2b1406b67310
Cyrus <cyong34@gatech.edu>
2025-04-10 17:55:15 -0400

    added users/ url

--- investing_project/urls.py
+++ investing_project/urls.py
@@ -21,5 +21,6 @@ from plaidapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('plaid/', include('plaidapp.urls')),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('users/', include('users.urls')),
 ]
