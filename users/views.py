e836daf82490cbd7d22f821cf5a619c13d6acb8b
Cyrus <cyong34@gatech.edu>
2025-04-14 14:18:37 -0400

    removed users since accounts is same

--- users/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.shortcuts import render, redirect
-
-# Create your views here.
-
