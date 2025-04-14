b8945696c1caf0367174d491457c490bcef13b2a
saigayathriprakash <saigayathri.prakash@gmail.com>
2025-04-14 11:49:22 -0400

    super basic welcome page and log in prompt!! signup url added!! home page added!!

--- investing_project/urls.py
+++ investing_project/urls.py
@@ -1,14 +1,12 @@
-from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect
+from django.contrib import admin
 
 from plaidapp.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('plaid/', include('plaidapp.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', home),
-    path('accounts/', include('django.contrib.auth.urls')),
-
+    path('', include('home.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),  # Built-in auth URLs
 ]
+
