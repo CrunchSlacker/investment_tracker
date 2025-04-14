5df1e6069400b8119f2855e32b9c222fd877bf03
Cyrus <cyong34@gatech.edu>
2025-04-14 13:29:25 -0400

    commented out signup views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
