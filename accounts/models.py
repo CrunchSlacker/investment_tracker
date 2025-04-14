3b49ec82c381dcd95bfb7e0d1169fbea0d88c7d3
Cyrus <cyong34@gatech.edu>
2025-04-14 17:32:55 -0400

    access token field is created when user signs up

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    access_token = models.TextField(blank=True, null=True)
+    item_id = models.CharField(max_length=255, blank=True, null=True)  # optional
+    
+    def __str__(self):
+        return self.user.username
